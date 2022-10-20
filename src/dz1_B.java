5e7cb6d0e1818945149e58ec902fce164ae4caed
seller <galiev.rasul.2002@yandex.ru>
2022-10-20 21:16:06 +0300

    dz1_b

--- src/dz1_B.java
+++ src/dz1_B.java
@@ -4,7 +4,6 @@ public class dz1_B {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-
         for (int i = 0; i < n; i++) {
             int size = scanner.nextInt();
             size++;
@@ -18,7 +17,7 @@ public class dz1_B {
             }
             my_sort(x, y);
             if (checkSort(y)) {
-                System.out.println("\nYES");
+                System.out.println("YES");
                 for (int j = 0; j < size - 1; j++) {
                     for (int k = x[j]; k < x[j + 1]; k++) {
                         System.out.print('R');
@@ -28,7 +27,7 @@ public class dz1_B {
                     }
                 }
             } else {
-                System.out.print("\nNO");
+                System.out.print("NO");
             }
         }
     }
@@ -57,4 +56,15 @@ public class dz1_B {
         for (i = 0; i < array.length - 2 && array[i] <= array[i + 1]; i++);
         return i == array.length - 2 && array[i] < array[i + 1];
     }
+    /* public static boolean checkSort(int[] array) {
+        boolean flag = true;
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }*/
+
 }
