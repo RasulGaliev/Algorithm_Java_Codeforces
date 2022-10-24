32c34297318767cd7730f57239b3787180c2a74a
seller <galiev.rasul.2002@yandex.ru>
2022-10-24 08:39:20 +0300

    dz1_D

---
+++ src/dz1_D.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class dz1_D {
+    public static void main(String[] args) {
+        List<Map<Integer, List<Integer>>> maps = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        int k = scanner.nextInt();
+        int num;
+        int size = k;
+        while(size-- > 0){
+            int n = scanner.nextInt();
+            Map<Integer, List<Integer>> map = new TreeMap<>();
+            for (int i = 0; i < n; i++) {
+                num = scanner.nextInt();
+                if (map.containsKey(num)){
+                    map.get(num).add(i);
+                } else {
+                    map.put(scanner.nextInt(), List.of(i));
+                }
+            }
+            maps.add(map);
+        }
+    }
+}
