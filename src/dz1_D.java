7734bb704a4be50a2374f3943dd60f919ddef745
seller <galiev.rasul.2002@yandex.ru>
2022-10-24 20:01:13 +0300

    dz1_G

--- src/dz1_D.java
+++ src/dz1_D.java
@@ -7,18 +7,70 @@ public class dz1_D {
         int k = scanner.nextInt();
         int num;
         int size = k;
-        while(size-- > 0){
+        List<Integer> sums = new ArrayList<>();
+        int sum;
+        while(size-- > 0) {
             int n = scanner.nextInt();
             Map<Integer, List<Integer>> map = new TreeMap<>();
-            for (int i = 0; i < n; i++) {
+            sum = 0;
+            for (int j = 0; j < n; j++) {
                 num = scanner.nextInt();
+                sum += num;
                 if (map.containsKey(num)){
-                    map.get(num).add(i);
+                    List<Integer> newArray = new ArrayList<>();
+                    for (int l = 0; l < map.get(num).size(); l++) {
+                        newArray.add(map.get(num).get(l));
+                    }
+                    newArray.add(j);
+                    map.put(num, newArray);
                 } else {
-                    map.put(scanner.nextInt(), List.of(i));
+                    map.put(num, List.of(j));
                 }
+
             }
+            sums.add(sum);
             maps.add(map);
+        }
+        boolean flag = false;
+        int thisKey;
+        int resultIndex1 = 0, resultIndex2 = 0;
+        int resultElem1 = 0, resultElem2 = 0;
+        for (int i = 0; i < k && !flag; i++) {
+            for (Map.Entry<Integer, List<Integer>> elem : maps.get(i).entrySet()) {
+                for (int j = i + 1; j < k; j++) {
+                    thisKey = elem.getKey()-(sums.get(i) - sums.get(j));
+                    if ((maps.get(j).containsKey(thisKey) && (maps.get(j).get(thisKey).size() > 1 || elem.getValue().size() > 1 ||
+                            !maps.get(j).get(thisKey).get(0).equals(elem.getValue().get(0))))) {
+                        // System.out.println("sum1 " + sums.get(i));
+                        // System.out.println("sum2 " + sums.get(j));
+                        // System.out.println("a1 " + elem.getKey());
+                        // System.out.println("thisKey " + thisKey);
+                        resultIndex1 = i + 1;
+                        resultIndex2 = j + 1;
+                        if (!maps.get(j).get(thisKey).get(0).equals(elem.getValue().get(0))) {
+                            resultElem2 = maps.get(j).get(thisKey).get(0) + 1;
+                            resultElem1 = elem.getValue().get(0) + 1;
+                        } else if(elem.getValue().size() > 1) {
+                            resultElem2 = maps.get(j).get(thisKey).get(0) + 1;
+                            resultElem1 = elem.getValue().get(2) + 1;
+                        } else {
+                            resultElem2 = maps.get(j).get(thisKey).get(1) + 1;
+                            resultElem1 = elem.getValue().get(0) + 1;
+                        }
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag)
+                    break;;
+            }
+        }
+        if (flag) {
+            System.out.println("YES");
+            System.out.println(resultIndex1 + " " + resultElem1);
+            System.out.println(resultIndex2 + " " + resultElem2);
+        } else {
+            System.out.println("NO");
         }
     }
 }
