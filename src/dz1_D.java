bfd6823365107123311d7078f8e1292eb90cb045
seller <galiev.rasul.2002@yandex.ru>
2022-10-25 10:04:58 +0300

    dz1_D

--- src/dz1_D.java
+++ src/dz1_D.java
@@ -1,75 +1,68 @@
 import java.util.*;
+import java.lang.Object;
 
 public class dz1_D {
     public static void main(String[] args) {
-        List<Map<Integer, List<Integer>>> maps = new ArrayList<>();
+        List<List<Integer>> lists = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         int k = scanner.nextInt();
         int num;
         int size = k;
         List<Integer> sums = new ArrayList<>();
         int sum;
-        while(size-- > 0) {
+        while (size-- > 0) {
             int n = scanner.nextInt();
-            Map<Integer, List<Integer>> map = new TreeMap<>();
+            List<Integer> list = new ArrayList<>();
             sum = 0;
             for (int j = 0; j < n; j++) {
                 num = scanner.nextInt();
                 sum += num;
-                if (map.containsKey(num)){
-                    List<Integer> newArray = new ArrayList<>();
-                    for (int l = 0; l < map.get(num).size(); l++) {
-                        newArray.add(map.get(num).get(l));
-                    }
-                    newArray.add(j);
-                    map.put(num, newArray);
-                } else {
-                    map.put(num, List.of(j));
-                }
-
+                list.add(num);
             }
             sums.add(sum);
-            maps.add(map);
+            lists.add(list);
         }
+
+
+        Map<Integer, Map<Integer, List<Integer>>> map = new TreeMap<>();
         boolean flag = false;
-        int thisKey;
-        int resultIndex1 = 0, resultIndex2 = 0;
-        int resultElem1 = 0, resultElem2 = 0;
         for (int i = 0; i < k && !flag; i++) {
-            for (Map.Entry<Integer, List<Integer>> elem : maps.get(i).entrySet()) {
-                for (int j = i + 1; j < k; j++) {
-                    thisKey = elem.getKey()-(sums.get(i) - sums.get(j));
-                    if ((maps.get(j).containsKey(thisKey) && (maps.get(j).get(thisKey).size() > 1 || elem.getValue().size() > 1 ||
-                            !maps.get(j).get(thisKey).get(0).equals(elem.getValue().get(0))))) {
-                        // System.out.println("sum1 " + sums.get(i));
-                        // System.out.println("sum2 " + sums.get(j));
-                        // System.out.println("a1 " + elem.getKey());
-                        // System.out.println("thisKey " + thisKey);
-                        resultIndex1 = i + 1;
-                        resultIndex2 = j + 1;
-                        if (!maps.get(j).get(thisKey).get(0).equals(elem.getValue().get(0))) {
-                            resultElem2 = maps.get(j).get(thisKey).get(0) + 1;
-                            resultElem1 = elem.getValue().get(0) + 1;
-                        } else if(elem.getValue().size() > 1) {
-                            resultElem2 = maps.get(j).get(thisKey).get(0) + 1;
-                            resultElem1 = elem.getValue().get(2) + 1;
-                        } else {
-                            resultElem2 = maps.get(j).get(thisKey).get(1) + 1;
-                            resultElem1 = elem.getValue().get(0) + 1;
+            for (int j = 0; j < lists.get(i).size() && !flag; j++) {
+                num = sums.get(i) - lists.get(i).get(j);
+                if (map.containsKey(num)) {
+                    for (Map.Entry<Integer, List<Integer>> elem : map.get(num).entrySet()) {
+                        if (elem.getKey() != i) {
+                            if (elem.getValue().get(0) != j || elem.getValue().size() > 1) {
+                                int resultIndex1 = i + 1, resultIndex2 = elem.getKey() + 1;
+                                int resultElem1 = j + 1;
+                                int resultElem2 = (elem.getValue().get(0));
+                                if (elem.getValue().get(0) == j)
+                                    resultElem2 = (elem.getValue().get(1));
+                                flag = true;
+                                System.out.println("sum1 " + sums.get(i));
+                                System.out.println("sum2 " + sums.get(j));
+                                // System.out.println("a1 " + elem.getKey());
+                                System.out.println("num " + num);
+                                System.out.println("YES");
+                                System.out.println(resultIndex1 + " " + resultElem1);
+                                System.out.println(resultIndex2 + " " + resultElem2 );
+                            }
+                        } else if (map.get(num).size() == 1) {
+                            List<Integer> newList = new ArrayList<>(map.get(num).get(i));
+                            //newList = (List<Integer>) map.get(num).get(i).clone();
+                            newList.add(j);
+                            Map<Integer, List<Integer>> newMap = new TreeMap<>(map.get(num));
+                            //newMap = (Map<Integer, List<Integer>>) map.get(num).clone();
+                            newMap.put(i, newList);
+                            map.put(num, newMap);
                         }
-                        flag = true;
-                        break;
                     }
+                } else {
+                    map.put(num, Map.of(i, List.of(j)));
                 }
-                if (flag)
-                    break;;
             }
         }
-        if (flag) {
-            System.out.println("YES");
-            System.out.println(resultIndex1 + " " + resultElem1);
-            System.out.println(resultIndex2 + " " + resultElem2);
-        } else {
+        if (!flag) {
             System.out.println("NO");
         }
     }
