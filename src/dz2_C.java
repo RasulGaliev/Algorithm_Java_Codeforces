b0249a56e4a127866a5d1516b332f2673af5101d
seller <galiev.rasul.2002@yandex.ru>
2022-11-10 21:49:45 +0300

    +dz2

--- src/dz2_C.java
+++ src/dz2_C.java
@@ -8,7 +8,7 @@ public class dz2_C {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int s = scanner.nextInt();
+        double s = scanner.nextDouble();
         double [][] matrix = new double[n][2];
         double  x, y;
         for (int i = 0; i < n; i++) {
@@ -17,17 +17,17 @@ public class dz2_C {
             matrix[i][0] = Math.sqrt(x * x + y * y);
             matrix[i][1] = scanner.nextDouble();
         }
-        for (int i = 0; i < n - 1; i++) {
-            for(int j = 0; j < n - i - 1; j++) {
-                if(matrix[j + 1][0] < matrix[j][0]) {
-                    double tmp = matrix[j][0];
-                    matrix[j][0] = matrix[j + 1][0];
-                    matrix[j + 1][0] = tmp;
+        boolean needIteration = true;
+        while (needIteration) {
+            needIteration = false;
+            for (int i = 1; i < n; i++) {
+                if (matrix[i][0] < matrix[i - 1][0]) {
+                    double[] tmp = matrix[i];
+                    matrix[i] = matrix[i - 1];
+                    matrix[i - 1] = tmp;
+                    needIteration = true;
                 }
             }
-        }
-        for (int i = 0; i < n; i++) {
-            System.out.println(matrix[i][0] + " " + matrix[i][1] + '\n');
         }
         if (s >= 1000000){
             System.out.println(0);
@@ -37,7 +37,7 @@ public class dz2_C {
         for (int i = 0; i < n; i++) {
             s += matrix[i][1];
             if (s >= 1000000){
-                System.out.println("*" + matrix[i][0]);
+                System.out.println(matrix[i][0]);
                 p = true;
                 break;
             }
