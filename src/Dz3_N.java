ead38684f926b8f09ab38091a6b7d43cea224bde
Rasul <galiev.rasul.2002@yandex.ru>
2022-12-03 20:37:30 +0300

    dz3_N

---
+++ src/Dz3_N.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Dz3_N {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int tmp;
+        Set<Integer>set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            tmp = scanner.nextInt();
+            if (tmp <= n && tmp >= 1)
+                set.add(tmp);
+        }
+        System.out.println(n - set.size());
+    }
+}
